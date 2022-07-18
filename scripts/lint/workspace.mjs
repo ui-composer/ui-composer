a7d07548b5b0b531bf5b4d3c2c56b96ba23fe7d2
Katherine Martinez <kmartinezmedia@gmail.com>
2022-07-18 17:28:44 -0400

    feat: add ui-composer cli package

--- scripts/lint/workspace.mjs
+++ scripts/lint/workspace.mjs
@@ -3,5 +3,6 @@
 const eslint = await $`yarn bin eslint`;
 
 cd(process.env.INIT_CWD);
+const config = argv.config ?? '.eslintrc.js';
 
-await $`yarn exec ${eslint} . --config .eslintrc.js`;
+await $`yarn exec ${eslint} . --config ${config} --max-warnings 0`;
